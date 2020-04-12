1d7670474dc4e1ec643bf9e42589ca50cde7f730
Guilherme Rodrigues <guilherme@vtex.com>
2020-04-12 19:55:41 -0300

    Meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Artur Personal Trainer Rio de Janeiro',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Personal Trainer profissional no Rio de Janeiro. Treinamento funcional, terceira idade e jovens. ',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
